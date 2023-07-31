6f8397c88e4fc342308df31bb580611bbdd73f99
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-08-01 03:09:19 +0500

    Add main.py draft

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+
+DATA_OPERATIONS_PATH = os.path.join('../package.json')
+QUANTITY_OF_OPERATIONS = 5
+
+
+def get_list_operations():
+    pass
+    # get_operations_data(path)
+    #
+    # get_executed_list(operations_data)
+    #
+    # get_sort_executed_list(executed_list)
+    #
+    # get_last_operations(operations_list, x)
+
