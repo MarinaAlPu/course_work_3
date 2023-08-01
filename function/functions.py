396ea45098df0ddcf64657936afc7ed3ce53b9d0
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-08-02 01:13:10 +0500

    Add docstrings in functions.py

--- function/functions.py
+++ function/functions.py
@@ -1,67 +1,71 @@
 import json
 from datetime import datetime
-# import os
-
-# DATA_OPERATIONS_PATH = os.path.join('../package.json')
-# print(DATA_OPERATIONS_PATH)
 
 
 def get_operations_data(path):
-    # operations_data = open('..', 'package.json')
-    # operations_data = json.load(operations_data)
+    """
+    Получает данные о транзакциях из файла .json
+    :param path: путь, по которому открывает файл для чтения
+    :return: данные о транзакциях из файла .json
+    """
     with open(path, 'r', encoding='UTF-8') as file:
         operations_data = json.load(file)
-        # print(operations_data)
-        # print(len(operations_data))
-        # print(type(operations_data))
-        # print(operations_data[0])
-        # print(type(operations_data[0]))
         return operations_data
 
 
-# operations_data = get_operations_data()
-
-
-def get_executed_list(operations_data):
+def get_executed_list(operations_data, operation_key):
+    """
+    Получает из списка операций список операций в соответствии с определённым статусом
+    :param operations_data: список всех операций
+    :param operation_key: статус операций
+    :return:  список операций с определённым статусом
+    """
     executed_list = []
     for operation in operations_data:
         for key, value in operation.items():
-            if operation[key] == 'EXECUTED':
+            if operation[key] == operation_key:
                 executed_list.append(operation)
-                # print(executed_list)
-                # return executed_list
-    # print(executed_list)
-    # print(len(executed_list))
     return executed_list
 
 
-# executed_list = get_executed_list(get_operations_data())
-
-
-def get_sort_executed_list(executed_list):
+def get_sort_executed_list(executed_list, param_for_sort):
+    """
+    Сортирует список выполненных операций по определённому параметру по убыванию
+    :param executed_list: список выполненных операций
+    :param param_for_sort: параметр, по которому сортируется список
+    :return: отсортированный по убыванию список выполненных операций
+    """
     def get_key_for_sort(operation):
-        # print(operation['date'])
-        return operation['date']
-    # print(sorted(executed_list, key=get_key_for_sort, reverse=True))
+        return operation[param_for_sort]
     return sorted(executed_list, key=get_key_for_sort, reverse=True)
 
 
-# get_sort_executed_list(get_executed_list(get_operations_data()))
-
-
 def get_last_operations(operations_list, quantity_of_operations):
-    # print(operations_list[0:5])
-    return operations_list[0:5]
+    """
+    Получает определённое количество последних операций
+    :param operations_list: отсортированный по убыванию список выполненных операций
+    :param quantity_of_operations: количество операций для вывода н аэкран
+    :return: список операций, которые будут выведена на экран
+    """
+    return operations_list[0:quantity_of_operations]
 
-
-# get_last_operations(get_sort_executed_list(get_executed_list(get_operations_data())), 5)
 
 def get_date_formatted(operation_date):
+    """
+    Форматирует дату, полученную из БД
+    :param operation_date: дата из БД
+    :return: дата в нужном формате
+    """
     date_formatted = datetime.strptime(operation_date, "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
     return date_formatted
 
 
 def get_from_formatted(operation):
+    """
+    Получает источник, из которого была проведена операция
+    :param operation: список операций
+    :return: откуда была проведена операция
+    """
     if 'from' in list(operation.keys()):
         if 'Счет' in operation['from']:
             from_where_value = operation['from'][-20:]
@@ -69,19 +73,23 @@ def get_from_formatted(operation):
             return f"{from_where} -> "
         else:
             from_where_value = operation['from'][-16:]
-            from_where = f"{operation['from'][:-16]} {from_where_value[0: 4]} {from_where_value[4: 6]}** **** {from_where_value[-4:]}"
+            from_where = f"{operation['from'][:-16]}{from_where_value[0: 4]} {from_where_value[4: 6]}** **** {from_where_value[-4:]}"
             return f"{from_where} -> "
     else:
         return ''
 
 
-
 def get_to_formatted(operation):
+    """
+    Получает адресата перевода
+    :param operation: список опреаций
+    :return: куда был осуществлён перевод
+    """
     if 'Счет' in operation['to']:
         to_value = operation['to'][-20:]
         to = f"{operation['to'][:-20]} **{to_value[-4:]}"
         return to
     else:
         to_value = operation['to'][-16:]
-        to = f"{operation['to'][:-16]} {to_value[0: 4]} {to_value[4: 6]}** **** {to_value[-4:]}"
+        to = f"{operation['to'][:-16]}{to_value[0: 4]} {to_value[4: 6]}** **** {to_value[-4:]}"
         return to
